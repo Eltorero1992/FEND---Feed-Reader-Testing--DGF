b18e1da4a81e674edb85381efcb1ab646b1ef871
Daniel Gomez Firmat <DanielGFirmat@gmail.com>
2019-03-05 18:47:36 +0000

    Improvements after assessment

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,10 +27,9 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /*The function below takes the allfeeds array
+        and analyzes one by one if they have an url and
+        are not empty*/
 
          it('has URL', function() {
            for(let feed of allFeeds){
@@ -40,10 +39,9 @@ $(function() {
          });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /*The function below takes the allfeeds array
+        and analyzes one by one if they have a name and
+        are not empty*/
 
         it('has name', function() {
            for(let feed of allFeeds){
@@ -54,23 +52,20 @@ $(function() {
 
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
+        /* The test suite below tests the behaviour of the slide menu */
 
 describe('the slide menu', function() {
 
         const hiddenMenu = document.querySelector('.slide-menu');
-        const menuIcon = document.querySelector('.menu-icon-link');
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
 
          it('is hidden by default', function(){
 
-            expect(hiddenMenu.parentElement.classList[0]).toBe('menu-hidden')
-            expect($(hiddenMenu).offset().left).toBe(-192)
+            /* Checks if the menu is in the hiding position before being clicked */
+
+            expect($(hiddenMenu).offset().left).toBe(-192);
+
+            /* Checks if the hidden class is on while the menu is in the hiding position */
+            expect($('body').hasClass('menu-hidden')).toBe(true);
 
          })
 })
@@ -81,14 +76,23 @@ describe('the slide menu', function() {
         const hiddenMenu = document.querySelector('.slide-menu');
         const menuIcon = document.querySelector('.menu-icon-link');
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+
+        /* I found two ways of tackling this test suite,
+
+            1. [Asynchronously] which will allow to check for the position of the test slide menu
+            and also to check when the 'menu-hidden' is toggle on and off of the
+            body
+
+            2. [Synchronously] Only allows to check whether the class menu hidden has been toggled
+            on and off from the body, as to check for the positioning of the menu will not be possible
+            as the animation is set on a 0.2s delay
+
+            */
 
 
-          /* This asynchronous function allows to check if menu has been displayed
+        /* Asynchronous */
+
+         /* This asynchronous function allows to check if menu has been displayed
           successfully after it has been clicked and the animation has finished
 
             [TODO | BUG ] Sometimes this test suite fails
@@ -104,96 +108,92 @@ describe('the slide menu', function() {
 
           */
 
-          beforeEach(function(done) {
-                menuIcon.click()
-                setTimeout(function(){
-                    done();
-                }, 201)
-          })
+        //   beforeEach(function(done) {
+        //         menuIcon.click()
+        //         setTimeout(function(){
+        //             done();
+        //         }, 201)
+        //   })
 
-          it('displays menu when clicked', function(done){
+        //   it('displays menu when clicked', function(done){
 
-            expect(hiddenMenu.parentElement.classList.length).toBe(0);
-            /* Checks for the position of the the menu after being clicked (Shown) */
-            expect($(hiddenMenu).offset().left).toBe(0)
-            done();
-        })
+        //     expect(hiddenMenu.parentElement.classList.length).toBe(0);
+        //     /* Checks for the position of the the menu after being clicked (Shown) */
+        //     expect($(hiddenMenu).offset().left).toBe(0)
+        //     expect($('body').hasClass('menu-hidden')).toBe(false);
+        //     done();
+        // })
 
-        it('hides when clicked again',function(done){
-            expect(hiddenMenu.parentElement.classList[0]).toBe('menu-hidden')
-            /* Checks for the position of the the menu after being clicked (Hidden) */
-            expect($(hiddenMenu).offset().left).toBe(-192)
-            done();
+        // it('hides when clicked again',function(done){
+        //     expect(hiddenMenu.parentElement.classList[0]).toBe('menu-hidden')
+        //     /* Checks for the position of the the menu after being clicked (Hidden) */
+        //     expect($(hiddenMenu).offset().left).toBe(-192)
+        //     expect($('body').hasClass('menu-hidden')).toBe(true);
+        //     done();
+
+        //   })
+
+        /* Synchrounously */
+
+        it('displays menu when clicked and hides when clicked again', function(){
+
+            menuIcon.click();
+
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            menuIcon.click();
+
+            expect($('body').hasClass('menu-hidden')).toBe(true);
 
           })
 
 })
 
-
-/* TODO: Write a new test suite named "Initial Entries" */
-
 describe ('Initial Entries', function() {
-
-        const feedContainer = document.querySelector('.feed')
 
         /* This function runs loadFeed and callsback once it has finished (2nd parameter in loadFeed) */
 
         beforeEach(function(done) {
-            loadFeed(0, (function () {
+            loadFeed(0, function () {
                 done();
-            }))
+            })
         })
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        /* Checks that the number of entries within the feed container is greater than 0 */
+
          it("feed container is not empty", function(done){
-            expect(feedContainer.childElementCount).toBeGreaterThan(0);
+            expect(document.querySelectorAll('.feed .entry').length).toBeGreaterThan(0);
             done();
          })
 
 })
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
 describe("New Feed Selection", function(){
 
-        const feedContainer = document.querySelector('.feed')
-        const feedEntry = feedContainer.children
+        const feedContainer = document.querySelector('.feed .entry')
+
+        let feedOne = "";
+        let feedTwo = "";
+
+        /* Asynchronous function to store two feeds in two separate
+        variables by checking the DOM and the entry links */
 
         beforeEach(function(done) {
-            loadFeed(0, (function () {
-                done();
-            }))
+            loadFeed(0, function () {
+                feedOne = document.querySelector('.feed .entry').parentElement;
+                loadFeed(1,function () {
+                    feedTwo = document.querySelector('.feed .entry').parentElement;
+                    done()
+                })
+            })
         })
 
-//      TODO: Write a test that ensures when a new feed is loaded
-//      * by the loadFeed function that the content actually changes.
-//      * Remember, loadFeed() is asynchronous.
+    /* Compares the first feed to the second one to not be equal */
 
-        it('Content changes when loaded',function(done){
-
-        expect(function () {
-
-            let counter = 0;
-
-                for (var i = 1; i <= feedContainer.childElementCount; i++) {
-
-                    if (feedEntry[i-1].innerText !== feedEntry[i]) {
-                        counter ++
-                    }
-                }
-
-            return counter
-
-        }()).toBe(10)
-
-        done();
-
-     })
+        it("should change content", function(done) {
+            expect(feedOne).not.toEqual(feedTwo)
+            done()
+        })
 
 })
 
